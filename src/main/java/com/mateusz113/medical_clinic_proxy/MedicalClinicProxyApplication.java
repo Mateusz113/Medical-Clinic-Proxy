a05c43fb784e5af3ab293ab16ef9a6870afe1e0f
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-10 18:41:07 +0200

    Initial commit

---
+++ src/main/java/com/mateusz113/medical_clinic_proxy/MedicalClinicProxyApplication.java
@@ -0,0 +1,11 @@
+package com.mateusz113.medical_clinic_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalClinicProxyApplication.class, args);
+    }
+}
