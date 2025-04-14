096d1dbee3ccab00128965c2b90898424b760201
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-14 15:36:23 +0200

    Update VisitClient.java
    Updated the name
    Updated to match the medical clinic visits controller

--- src/main/java/com/mateusz113/medical_clinic_proxy/client/VisitClient.java
+++ src/main/java/com/mateusz113/medical_clinic_proxy/client/VisitClient.java
@@ -1,39 +1,24 @@
 package com.mateusz113.medical_clinic_proxy.client;
 
-import com.mateusz113.medical_clinic_proxy.config.FeignConfig;
+import com.mateusz113.medical_clinic_proxy.filter.visit.ExternalVisitFilter;
 import com.mateusz113.medical_clinic_proxy.model.PageableContentDto;
 import com.mateusz113.medical_clinic_proxy.model.visit.VisitDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.OffsetDateTime;
-
-@FeignClient(value = "medical-clinic-visits", url = "medical-clinic:8080/visits")
+@FeignClient(value = "visits-client", url = "medical-clinic:8080/visits")
 public interface VisitClient {
     @GetMapping
-    PageableContentDto<VisitDto> getDoctorVisits(
-            @RequestParam Long doctorId,
-            Pageable pageable,
-            @RequestParam Boolean onlyAvailable
-    );
-
-    @GetMapping
-    PageableContentDto<VisitDto> getDoctorSpecializationVisits(
-            @RequestParam("doctorSpecialization") String doctorSpecialization,
-            Pageable pageable,
-            @RequestParam Boolean onlyAvailable,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime startTime,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime endTime
-    );
+    PageableContentDto<VisitDto> getVisits(@SpringQueryMap ExternalVisitFilter visitFilter, Pageable pageable);
 
     @GetMapping
     PageableContentDto<VisitDto> getPatientVisits(@RequestParam Long patientId, Pageable pageable);
 
-    @PatchMapping("/{visitId}/patient")
-    void registerPatientToVisit(@PathVariable Long visitId, @RequestParam Long patientId);
+    @PatchMapping("/{visitId}/patient/{patientId}")
+    void registerPatientToVisit(@PathVariable("visitId") Long visitId, @PathVariable("patientId") Long patientId);
 }
