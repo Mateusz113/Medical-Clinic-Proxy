a7f5854bb7e107cd17d064396874b8032be8c026
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-14 15:38:56 +0200

    Update VisitController.java
    Updated controller to match the medical clinic

--- src/main/java/com/mateusz113/medical_clinic_proxy/controller/VisitController.java
+++ src/main/java/com/mateusz113/medical_clinic_proxy/controller/VisitController.java
@@ -1,21 +1,18 @@
 package com.mateusz113.medical_clinic_proxy.controller;
 
+import com.mateusz113.medical_clinic_proxy.filter.visit.InternalVisitFilter;
 import com.mateusz113.medical_clinic_proxy.model.PageableContentDto;
 import com.mateusz113.medical_clinic_proxy.model.visit.VisitDto;
 import com.mateusz113.medical_clinic_proxy.service.VisitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/visits")
@@ -23,31 +20,22 @@ import java.time.LocalDate;
 public class VisitController {
     private final VisitService visitService;
 
-    @GetMapping("/doctor/id")
-    public PageableContentDto<VisitDto> getDoctorVisits(
-            @RequestParam Long doctorId,
+    @GetMapping
+    public PageableContentDto<VisitDto> getVisits(
+            InternalVisitFilter internalVisitFilter,
             Pageable pageable
     ) {
-        return visitService.getDoctorVisits(doctorId, pageable);
+        return visitService.getVisits(internalVisitFilter, pageable);
     }
 
-    @GetMapping("/doctor/specialization")
-    public PageableContentDto<VisitDto> getDoctorSpecializationVisits(
-            @RequestParam String doctorSpecialization,
-            Pageable pageable,
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate visitDate
-    ) {
-        return visitService.getDoctorSpecializationVisits(doctorSpecialization, pageable, visitDate);
-    }
-
-    @GetMapping("/patient")
-    PageableContentDto<VisitDto> getPatientVisits(@RequestParam Long patientId, Pageable pageable) {
+    @GetMapping("/patient/{patientId}")
+    public PageableContentDto<VisitDto> getPatientVisits(@PathVariable Long patientId, Pageable pageable) {
         return visitService.getPatientVisits(patientId, pageable);
     }
 
-    @PatchMapping("/{visitId}/patient")
+    @PatchMapping("/{visitId}/patient/{patientId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void registerPatientToVisit(@PathVariable Long visitId, @RequestParam Long patientId) {
+    void registerPatientToVisit(@PathVariable("visitId") Long visitId, @PathVariable("patientId") Long patientId) {
         visitService.registerPatientToVisit(visitId, patientId);
     }
 }
