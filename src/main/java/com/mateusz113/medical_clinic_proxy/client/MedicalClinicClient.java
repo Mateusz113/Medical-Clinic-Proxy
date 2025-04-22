07949dc18ac583c30f53c622a4ec18227e1698c7
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-22 11:07:52 +0200

    Update MedicalClinicClient.java
    Updated client name and config
    Updated endpoint path or registering patients

--- src/main/java/com/mateusz113/medical_clinic_proxy/client/MedicalClinicClient.java
+++ src/main/java/com/mateusz113/medical_clinic_proxy/client/MedicalClinicClient.java
@@ -1,5 +1,6 @@
 package com.mateusz113.medical_clinic_proxy.client;
 
+import com.mateusz113.medical_clinic_proxy.config.MedicalClinicClientConfig;
 import com.mateusz113.medical_clinic_proxy.filter.visit.ExternalVisitFilter;
 import com.mateusz113.medical_clinic_proxy.model.PageableContentDto;
 import com.mateusz113.medical_clinic_proxy.model.visit.VisitDto;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "visits-client", url = "${spring.cloud.openfeign.client.config.visits-client.url}")
+@FeignClient(name = "medical-clinic-client", url = "${spring.cloud.openfeign.client.config.medical-clinic-client.url}", configuration = MedicalClinicClientConfig.class)
 public interface MedicalClinicClient {
     @GetMapping("/visits")
     PageableContentDto<VisitDto> getVisits(@SpringQueryMap ExternalVisitFilter visitFilter, Pageable pageable);
@@ -19,6 +20,6 @@ public interface MedicalClinicClient {
     @GetMapping("/visits")
     PageableContentDto<VisitDto> getPatientVisits(@RequestParam Long patientId, Pageable pageable);
 
-    @PatchMapping("/{visitId}/patient/{patientId}")
+    @PatchMapping("/visits/{visitId}/patient/{patientId}")
     void registerPatientToVisit(@PathVariable("visitId") Long visitId, @PathVariable("patientId") Long patientId);
 }
