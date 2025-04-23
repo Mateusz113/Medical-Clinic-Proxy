ee7eb89e91af5f288d40ec14483c354a52fa1542
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-23 22:35:32 +0200

    Update PageableContentDto.java
    Added builder

--- src/main/java/com/mateusz113/medical_clinic_proxy/model/PageableContentDto.java
+++ src/main/java/com/mateusz113/medical_clinic_proxy/model/PageableContentDto.java
@@ -1,7 +1,10 @@
 package com.mateusz113.medical_clinic_proxy.model;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record PageableContentDto<S>(
         long totalEntries,
         int totalNumberOfPages,
