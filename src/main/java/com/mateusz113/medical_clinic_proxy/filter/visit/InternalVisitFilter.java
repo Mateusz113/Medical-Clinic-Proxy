1c314a5a5d3d3662d94521d1641146eb829f6a27
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-25 16:25:29 +0200

    Update InternalVisitFilter.java
    Added builder

--- src/main/java/com/mateusz113/medical_clinic_proxy/filter/visit/InternalVisitFilter.java
+++ src/main/java/com/mateusz113/medical_clinic_proxy/filter/visit/InternalVisitFilter.java
@@ -1,9 +1,11 @@
 package com.mateusz113.medical_clinic_proxy.filter.visit;
 
+import lombok.Builder;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@Builder
 public record InternalVisitFilter(
         Long visitId,
         Long doctorId,
